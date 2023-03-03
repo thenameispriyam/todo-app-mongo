bbb93897f74a958f31182176dae815bd9d94c50c
Priyam Singh <priyam0singh@gmail.com>
2023-03-04 01:44:45 +0530

    add project files

---
+++ routes/todo.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const Todo = require("../models/Todo");
+
+// routes
+router
+  .post("/add/todo", (req, res) => {
+    const { todo } = req.body;
+    const newTodo = new Todo({ todo });
+
+    // save the todo
+    newTodo
+      .save()
+      .then(() => {
+        console.log(" 1 New Entry ");
+        res.redirect("/");
+      })
+      .catch((err) => console.log(err));
+  })
+
+  .get("/delete/todo/:_id", (req, res) => {
+    const { _id } = req.params;
+    Todo.deleteOne({ _id })
+      .then(() => {
+        console.log(" Deleted 1 Entry ");
+        res.redirect("/");
+      })
+      .catch((err) => console.log(err));
+  });
+
+module.exports = router;
